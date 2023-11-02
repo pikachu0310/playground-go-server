0f922a353fad0673035aef2a642687e4e329a0f0
pikachu0310 <sunnon@i.softbank.jp>
2023-11-02 17:40:57 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"go-backend-sample/internal/handler"
+	"go-backend-sample/internal/migration"
+	"go-backend-sample/internal/pkg/config"
+	"go-backend-sample/internal/repository"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// middlewares
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
+	// connect to database
+	db, err := sqlx.Connect("mysql", config.MySQL().FormatDSN())
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	defer db.Close()
+
+	// migrate tables
+	if err := migration.MigrateTables(db.DB); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	// setup repository
+	repo := repository.New(db)
+
+	// setup routes
+	h := handler.New(repo)
+	v1API := e.Group("/api/v1")
+	h.SetupRoutes(v1API)
+
+	e.Logger.Fatal(e.Start(config.AppAddr()))
+}
